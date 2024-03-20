f6a4e695552126c15920cd8b7f00f4ff8ec7bf70
shiku-kimani <161102735+shiku-kimani@users.noreply.github.com>
2024-03-20 23:32:57 +0300

    Add files via upload

---
+++ numbers.dart
@@ -0,0 +1,11 @@
+void main() {
+  print('given number:7');
+  int input = 7;
+  if (input > 10) {
+    print('your number is greater than 10');
+  } else if (input < 10) {
+    print('Your number is less than 10');
+  } else
+    (input = 10);
+  print('Your number is equal to 10');
+}
